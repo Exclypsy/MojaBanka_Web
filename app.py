26fd3d1155005ed07445eb7df1d4eb2a88e23fc5
sonamihalikova <sonamihalikova7@gmail.com>
2025-11-19 08:48:11 +0100

    Sonicka navarila

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 
-#test
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('heslo')
+        print(email, password)
+        return f"Data boli prijate na spracovanie"
+    return render_template("HlavnyWeb.html")
+
+
+@app.route('/fnfn/')
+def AhojSvet():  # put application's code here
+    return 'Ahoj svet!!!'
 
 
 if __name__ == '__main__':
     app.run()
+
