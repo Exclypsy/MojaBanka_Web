ca85b7e1230085551995f60a0bde795c1531b286
sonamihalikova <sonamihalikova7@gmail.com>
2025-12-13 14:15:15 +0100

    prepojenie s db funkcne

---
+++ db_banka4SB/main_test_db.py
@@ -0,0 +1,15 @@
+from db import get_connection
+
+def main():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM klient")
+    count = cursor.fetchone()[0]
+    print("Pocet klientov v DB:", count)
+
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
